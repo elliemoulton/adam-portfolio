eaf807d2d30e1887a48307a7d8032dd4041b7d7c
Ellie Moulton <elysermoulton@gmail.com>
2025-05-09 17:36:27 +0200

    initial commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <>
+            <Head>
+                <link
+                    rel='preload'
+                    href='/fonts/PPNeueMontreal-Book.woff2'
+                    as='font'
+                    type='font/woff2'
+                    crossOrigin='anonymous'
+                />
+            </Head>
+            <header className='dark-background'>
+                <nav>
+                    <p>
+                        <Link href='/' className='logo'>
+                            Moulton / Marketing
+                        </Link>
+                    </p>
+                    <ul>
+                        <li>
+                            <Link href='/'>Our Services</Link>
+                        </li>
+                        <li>
+                            <Link href='/'>Pricing</Link>
+                        </li>
+                        <li>
+                            <Link href='/'>Case Studies</Link>
+                        </li>
+                        <li>
+                            <Link href='/'>About</Link>
+                        </li>
+                        <li>
+                            <Link href='/' className='contact'>Contact Us</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
