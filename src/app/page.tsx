25104d905e285c7dc4f6613e494f5f31ed07af35
Ellie Moulton <elysermoulton@gmail.com>
2025-05-19 18:05:38 +0200

    contact block on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import ImageCarousel from './components/ImageCarousel';
 import CaseStudiesCards from './components/CaseStudiesCards';
@@ -7,7 +8,7 @@ export default function Home() {
     return (
         <>
             <div className='dark-background hero-section'>
-                <div className='hero-section-upper'>
+                <div className='hero-section-upper content'>
                     <div className='left-column'>
                         <h1>Get More From Your Website</h1>
                         <p>
@@ -37,42 +38,131 @@ export default function Home() {
                     <ImageCarousel />
                 </div>
             </div>
-            <div className='light-background our-services-block'>
-                <div className='text-block'>
-                    <h2>Our Services</h2>
-                    <h3>Web Design & Development</h3>
-                    <p>
-                        We create visually compelling, responsive websites that
-                        not only look great but are built to perform. From
-                        design to development, we ensure your site aligns with
-                        your brand and meets your business goals.
-                    </p>
-                    <Link href='/'>Learn More &nbsp; &rarr;</Link>
-                    <h3>SEO & Content Marketing</h3>
-                    <p>
-                        Our SEO strategies are designed to increase your online
-                        visibility and drive qualified users to your site. We
-                        optimize your site&#39;s content, structure, and
-                        technical elements to improve search engine rankings.
-                    </p>
-                    <Link href='/'>Learn More &nbsp; &rarr;</Link>
-                    <h3>Web Design & Development</h3>
-                    <p>
-                        We focus on enhancing the user experience by making your
-                        website intuitive, easy to navigate, and engaging. Our
-                        goal is to create seamless interactions that keep users
-                        satisfied and encourage conversions.
-                    </p>
-                    <Link href='/'>Learn More &nbsp; &rarr;</Link>
+            <div className='light-background'>
+                <div className='our-services-block content'>
+                    <div className='text-block'>
+                        <h2>Our Services</h2>
+                        <h3>Web Design & Development</h3>
+                        <p>
+                            We create visually compelling, responsive websites
+                            that not only look great but are built to perform.
+                            From design to development, we ensure your site
+                            aligns with your brand and meets your business
+                            goals.
+                        </p>
+                        <Link href='/'>Learn More &nbsp; &rarr;</Link>
+                        <h3>SEO & Content Marketing</h3>
+                        <p>
+                            Our SEO strategies are designed to increase your
+                            online visibility and drive qualified users to your
+                            site. We optimize your site&#39;s content,
+                            structure, and technical elements to improve search
+                            engine rankings.
+                        </p>
+                        <Link href='/'>Learn More &nbsp; &rarr;</Link>
+                        <h3>Web Design & Development</h3>
+                        <p>
+                            We focus on enhancing the user experience by making
+                            your website intuitive, easy to navigate, and
+                            engaging. Our goal is to create seamless
+                            interactions that keep users satisfied and encourage
+                            conversions.
+                        </p>
+                        <Link href='/'>Learn More &nbsp; &rarr;</Link>
+                    </div>
                 </div>
             </div>
-            <div className='dark-background case-studies-block'>
-                <h2>
-                    Learn more about how we work. Explore case studies from some
-                    of our latest projects.
-                </h2>
-                <CaseStudiesCards />
+            <div className='dark-background'>
+                <div className='case-studies-block content'>
+                    <h2>
+                        Learn more about how we work. Explore case studies from
+                        some of our latest projects.
+                    </h2>
+                    <CaseStudiesCards />
                     <Link href='/'>View All Case Studies &nbsp; &rarr;</Link>
+                </div>
+                <div className=' about-block content'>
+                    <h2>ABOUT MOULTON MARKETING</h2>
+                    <div className='about-block-content'>
+                        <div className='text-block'>
+                            <p>
+                                Moulton Marketing is a boutique SEO, UX, and web
+                                design agency based in Amsterdam, dedicated to
+                                helping businesses enhance their online presence
+                                with smart, data-driven strategies.
+                                <br />
+                                <br />
+                                Founded by Adam Moulton after a successful
+                                career in the U.S. at some of the top
+                                performance marketing agencies, the company
+                                brings a global perspective to digital
+                                marketing.
+                                <br />
+                                <br />
+                                Adam&#39;s passion for travel and freelancing
+                                inspired him to create an agency that blends
+                                creativity, technical expertise, and
+                                user-focused design to deliver impactful
+                                results.
+                            </p>
+                        </div>
+                        <Image
+                            src='/imgs/profile.svg'
+                            alt='Adam Moulton Profile'
+                            width={509}
+                            height={600}
+                        />
+                    </div>
+                </div>
+                <div className='content contact-block'>
+                    {/* <Image
+                        src='/imgs/getintouch-desktop.svg'
+                        alt='Get in Touch'
+                        width={1320}
+                        height={791}
+                    /> */}
+                    <form action='/' method='POST' className='light-background'>
+                        <div className='form-content'>
+                            <h2>Contact Us</h2>
+                            <label htmlFor='name'>Name</label>
+                            <input
+                                type='text'
+                                id='name'
+                                name='name'
+                                className='input'
+                                required
+                            />
+                            <label htmlFor='email'>Email</label>
+                            <input
+                                type='email'
+                                id='email'
+                                name='email'
+                                className='input'
+                                required
+                            />
+                            <label htmlFor='message'>How Can We Help?</label>
+                            <select
+                                id='message'
+                                name='message'
+                                className='input'
+                                required
+                            >
+                                <option value='' disabled selected>
+                                    {' '}
+                                </option>
+                                <option value='Web Design'>Web Design</option>
+                                <option value='SEO'>SEO</option>
+                                <option value='Content Marketing'>
+                                    Content Marketing
+                                </option>
+                                <option value='Other'>Other</option>
+                            </select>
+                            <div>
+                                <button type='submit'>SUBMIT</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     );
