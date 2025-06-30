78a73fb81be02d8bb26e9cd568d8398dc05c9b25
Ellie Moulton <elysermoulton@gmail.com>
2025-06-30 17:00:22 +0200

    update

--- src/app/components/HomepageCaseStudiesCards.tsx
+++ src/app/components/HomepageCaseStudiesCards.tsx
@@ -6,6 +6,7 @@ import { caseStudiesData } from '../data/casestudiesdata';
 export default function CaseStudiesCards() {
     return (
         <>
+            {/* [0,1].map makes it go through the studies 2x and make 2 rows of 2 */}
             {[0, 1].map((rowIndex) => (
                 <div className={styles.row} key={rowIndex}>
                     {caseStudiesData
@@ -33,55 +34,6 @@ export default function CaseStudiesCards() {
                         ))}
                 </div>
             ))}
-
-            {/* old code... new code with [0,1].map makes it go through the studies 2x and make 2 rows of 2 */}
-            {/* 
-            <div className={styles.row}>
-                {caseStudiesData.slice(0, 2).map((study) => (
-                    <Link
-                        href={`/case-studies/${study.slug}`}
-                        key={study.slug}
-                        className={styles.card}
-                    >
-                        <div>
-                            <Image
-                                src={study.homepageImage.src}
-                                width={study.homepageImage.width}
-                                height={study.homepageImage.height}
-                                alt={study.homepageImage.alt}
-                                priority
-                            />
-                        </div>
-                        <div className={styles.textWrapper}>
-                            <h3>{study.title}</h3>
-                            <p>{study.subtitle}</p>
-                        </div>
-                    </Link>
-                ))}
-            </div>
-            <div className={styles.row}>
-                {caseStudiesData.slice(2, 4).map((study) => (
-                    <Link
-                        href={`/case-studies/${study.slug}`}
-                        key={study.slug}
-                        className={styles.card}
-                    >
-                        <div>
-                            <Image
-                                src={study.homepageImage.src}
-                                width={study.homepageImage.width}
-                                height={study.homepageImage.height}
-                                alt={study.homepageImage.alt}
-                                priority
-                            />
-                        </div>
-                        <div className={styles.textWrapper}>
-                            <h3>{study.title}</h3>
-                            <p>{study.subtitle}</p>
-                        </div>
-                    </Link>
-                ))}
-            </div> */}
         </>
     );
 }
