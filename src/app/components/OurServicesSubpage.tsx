778efcaced920141d3c017bf1c2dfa48181ea9de
Ellie Moulton <elysermoulton@gmail.com>
2025-08-06 15:08:41 +0200

    adding tech stack images and styling our services pages

--- src/app/components/OurServicesSubpage.tsx
+++ src/app/components/OurServicesSubpage.tsx
@@ -1,4 +1,15 @@
 import styles from '../styles/OurServicesSubpage.module.css';
+import Image from 'next/image';
+
+const techStackImages = [
+    { src: '/imgs/tech-stack/figma.svg', alt: 'Figma' },
+    { src: '/imgs/tech-stack/react.svg', alt: 'React' },
+    { src: '/imgs/tech-stack/nextjs.svg', alt: 'Next.js' },
+    { src: '/imgs/tech-stack/elementor.svg', alt: 'Elementor' },
+    { src: '/imgs/tech-stack/wordpress.svg', alt: 'WordPress' },
+    { src: '/imgs/tech-stack/shopify.svg', alt: 'Shopify' },
+    { src: '/imgs/tech-stack/squarespace.svg', alt: 'Squarespace' },
+];
 
 interface OurServicesSubpageProps {
     title: string;
@@ -15,12 +26,29 @@ export default function OurServicesSubpage(props: OurServicesSubpageProps) {
             <div className={styles.ourServicesSubpage}>
                 <h1>{props.title}</h1>
                 <h2>{props.subtitle}</h2>
-                {[...props.services].map((service, index) => (
-                    <div key={index}>
-                        <h3>{service.title}</h3>
-                        <p>{service.description}</p>
+                <div className={styles.services}>
+                    {[...props.services].map((service, index) => (
+                        <div key={index} className={styles.service}>
+                            <h3>{service.title}</h3>
+                            <p>{service.description}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.techStack}>
+                    <h2>Our Tech Stack</h2>
+                    <div className={styles.techStackImages}>
+                        {techStackImages.map((image, index) => (
+                            <Image
+                                key={index}
+                                src={image.src}
+                                alt={image.alt}
+                                width={0}
+                                height={90}
+                                className={styles.techStackImage}
+                            />
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         </>
     );
